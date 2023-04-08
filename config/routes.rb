8b65409a3f5630ef22ff76ac1e02429011b04457
adnrbp <adnrbp@users.noreply.github.com>
2023-04-08 17:59:35 -0500

    refactor: #11 add autocomplete filter api to auth only

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'cities/search'
-  
   devise_for :users
 
   devise_scope :user do
     authenticated :user do
       root 'home#index', as: :authenticated_root
+      get 'cities/search'
     end
   
     unauthenticated do
